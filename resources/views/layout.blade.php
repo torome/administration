6df3531ec3fe9085ad968251c7383a4d674eb80c
twilroad <269044570@qq.com>
2016-11-04 19:04:18 +0800

    添加SEO信息的输出

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Notadd后台管理</title>
+    <title>{{ set('title') }}</title>
     <meta name="description" content="Notadd">
     <meta name="keyword" content="Notadd">
     @section('css')@show
