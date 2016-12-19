9ecc3c893331c892bd556cb008c387ac7b7f5046
twilroad <269044570@qq.com>
2016-12-19 19:43:33 +0800

    add require js file app.js

--- resources/assets/src/main.js
+++ resources/assets/src/main.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'font-awesome/less/font-awesome.less'
-import 'bootstrap'
-import './assets/main.less'
-import './assets/skins/all-skins.less'
+import 'bootstrap/less/bootstrap.less'
+import 'slimscroll/example/ssmaster/jquery.slimscroll'
+import './assets/less/main.less'
+import './assets/less/skins/all-skins.less'
+import './assets/js/app'
 import Vue from 'vue'
 import App from './components/App'
 import router from './router'
