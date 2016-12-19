c7d664e0215b87026637255b9ce1b77dc6b4725f
twilroad <269044570@qq.com>
2016-12-19 15:47:14 +0800

    add styles to main.js

--- resources/assets/src/main.js
+++ resources/assets/src/main.js
@@ -1,5 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'font-awesome/less/font-awesome.less'
+import 'bootstrap'
+import './assets/main.less'
+import './assets/skins/all-skins.less'
 import Vue from 'vue'
 import App from './components/App'
 import router from './router'
