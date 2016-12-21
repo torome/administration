09e2bc2b747065326e3efa314e1b52872c4f30eb
twilroad <269044570@qq.com>
2016-12-21 18:22:01 +0800

    fix logo area text

--- resources/assets/src/components/layouts/LayoutHeader.vue
+++ resources/assets/src/components/layouts/LayoutHeader.vue
@@ -4,8 +4,8 @@
 <template>
     <header class="main-header">
         <router-link class="logo" to="/">
-            <span class="logo-mini"><b>N</b>A</span>
-            <span class="logo-lg"><b>Not</b>Add</span>
+            <span class="logo-mini">N</span>
+            <span class="logo-lg">NotAdd</span>
         </router-link>
         <nav class="navbar navbar-static-top" role="navigation">
             <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
