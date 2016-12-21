ce1eb4bc5998b36253de35d5db9b05312ca40127
twilroad <269044570@qq.com>
2016-12-21 15:46:24 +0800

    add message show template

--- resources/assets/src/components/pages/Setting.vue
+++ resources/assets/src/components/pages/Setting.vue
@@ -1,11 +1,22 @@
 <script>
-  export default {}
+  export default {
+    methods: {
+      show: function () {
+        this.$store.commit('message', {
+          show: true,
+          type: 'success',
+          text: '这是一个成功消息'
+        })
+      }
+    }
+  }
 </script>
 <style></style>
 <template>
     <div class="box">
         <div class="box-header with-border">
             <h3 class="box-title">全局设置</h3>
+            <span @click="show">显示信息</span>
         </div>
     </div>
 </template>
