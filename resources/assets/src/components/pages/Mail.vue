109a4f6584a91226351521c8ff6d75c44e235a1e
twilroad <269044570@qq.com>
2016-12-21 22:47:25 +0800

    refactor v-validate with validity in component Mail

--- resources/assets/src/components/pages/Mail.vue
+++ resources/assets/src/components/pages/Mail.vue
@@ -13,42 +13,54 @@
         <validation name="validation">
             <div class="form-horizontal">
                 <div class="box-body">
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">协议</label>
-                        <div class="col-sm-4">
-                            <input v-model="protocol" type="text" class="form-control" placeholder="请输入协议" v-validate:protocol="{ required: { rule: true, message: '协议不能为空' } }">
+                    <validity field="protocol" :validators="{ required: { rule: true, message: '协议不能为空' } }">
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">协议</label>
+                            <div class="col-sm-4">
+                                <input v-model="protocol" type="text" class="form-control" placeholder="请输入协议">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">加密方式</label>
-                        <div class="col-sm-4">
-                            <input v-model="encryption" type="text" class="form-control" placeholder="请输入加密方式" v-validate:encryption="{ required: { rule: true, message: '加密方式不能为空' } }">
+                    </validity>
+                    <validity field="encryption" :validators="{ required: { rule: true, message: '加密方式不能为空' } }">
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">加密方式</label>
+                            <div class="col-sm-4">
+                                <input v-model="encryption" type="text" class="form-control" placeholder="请输入加密方式">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">主机</label>
-                        <div class="col-sm-4">
-                            <input v-model="host" type="text" class="form-control" placeholder="请输入主机地址" v-validate:host="{ required: { rule: true, message: '主机地址不能为空' } }">
+                    </validity>
+                    <validity field="host" :validators="{ required: { rule: true, message: '主机地址不能为空' } }">
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">主机</label>
+                            <div class="col-sm-4">
+                                <input v-model="host" type="text" class="form-control" placeholder="请输入主机地址">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">邮箱</label>
-                        <div class="col-sm-4">
-                            <input v-model="mail" type="text" class="form-control" placeholder="请输入邮箱名称" v-validate:mail="{ required: { rule: true, message: '邮箱名称不能为空' } }">
+                    </validity>
+                    <validity field="mail" :validators="{ required: { rule: true, message: '邮箱名称不能为空' } }">
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">邮箱</label>
+                            <div class="col-sm-4">
+                                <input v-model="mail" type="text" class="form-control" placeholder="请输入邮箱名称">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">用户名</label>
-                        <div class="col-sm-4">
-                            <input v-model="username" type="text" class="form-control" placeholder="请输入用户名称" v-validate:username="{ required: { rule: true, message: '用户名称不能为空' } }">
+                    </validity>
+                    <validity field="username" :validators="{ required: { rule: true, message: '用户名称不能为空' } }">
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">用户名</label>
+                            <div class="col-sm-4">
+                                <input v-model="username" type="text" class="form-control" placeholder="请输入用户名称">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">密码</label>
-                        <div class="col-sm-4">
-                            <input v-model="password" type="password" class="form-control" placeholder="请输入密码" v-validate:password="{ required: { rule: true, message: '密码不能为空' } }">
+                    </validity>
+                    <validity field="password" :validators="{ required: { rule: true, message: '密码不能为空' } }">
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">密码</label>
+                            <div class="col-sm-4">
+                                <input v-model="password" type="password" class="form-control" placeholder="请输入密码">
+                            </div>
                         </div>
-                    </div>
+                    </validity>
                 </div>
                 <div class="box-footer">
                     <div class="form-group">
