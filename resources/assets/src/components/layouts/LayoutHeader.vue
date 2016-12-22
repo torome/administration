bc6e9879cce19cdeef317478621bb09b3a164e01
twilroad <269044570@qq.com>
2016-12-22 16:08:16 +0800

    custom small logo and big logo style in sidebar

--- resources/assets/src/components/layouts/LayoutHeader.vue
+++ resources/assets/src/components/layouts/LayoutHeader.vue
@@ -1,11 +1,26 @@
 <script>
-    export default {}
+  export default {}
 </script>
+<style scoped>
+    .logo-mini {
+        background: url(../../../static/images/logo.svg);
+        height: 90px;
+        margin-left: -15px !important;
+        width: 90px;
+    }
+    .logo-lg {
+        background: url("../../../static/images/logo-big.svg") 20px center no-repeat;
+        height: 90px;
+        padding-left: 170px;
+        padding-top: 5px;
+        text-align: left;
+    }
+</style>
 <template>
     <header class="main-header">
         <router-link class="logo" to="/">
-            <span class="logo-mini">N</span>
-            <span class="logo-lg">NotAdd</span>
+            <span class="logo-mini"></span>
+            <span class="logo-lg">管理后台</span>
         </router-link>
         <nav class="navbar navbar-static-top" role="navigation">
             <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
