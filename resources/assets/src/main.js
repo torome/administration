412c8dfd33c102363375f769a0e116dab2daf670
twilroad <269044570@qq.com>
2016-12-22 17:15:59 +0800

    replace vue-validator with vee-validator

--- resources/assets/src/main.js
+++ resources/assets/src/main.js
@@ -7,13 +7,13 @@ import './assets/less/main.less'
 import './assets/less/skins/all-skins.less'
 import '../static/js/app'
 import Vue from 'vue'
-import VueValidator from 'vue-validator'
+import VeeValidate from 'vee-validate'
 import VueResource from 'vue-resource'
 import App from './components/App'
 import router from './router'
 import store from './stores'
 
-Vue.use(VueValidator)
+Vue.use(VeeValidate)
 Vue.use(VueResource)
 
 /* eslint-disable no-new */
