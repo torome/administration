ba60f1211c934e7e966d843a4d0c27015176ada2
twilroad <269044570@qq.com>
2016-12-22 20:15:52 +0800

    correct error text in component Setting

--- resources/assets/src/components/pages/Setting.vue
+++ resources/assets/src/components/pages/Setting.vue
@@ -46,7 +46,7 @@
                         <input name="name" type="text" class="form-control" placeholder="请输入网站名称" v-model="name" v-validate data-vv-rules="required">
                     </div>
                     <div class="col-sm-5">
-                        <span class="help-block" v-show="errors.has('name')">{{ errors.first('name') }}</span>
+                        <span class="help-block" v-show="errors.has('name')">网站名称不能为空</span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('domain') }">
@@ -55,7 +55,7 @@
                         <input name="domain" type="text" class="form-control" placeholder="请输入网站域名" v-model="domain" v-validate data-vv-rules="required">
                     </div>
                     <div class="col-sm-5">
-                        <span class="help-block" v-show="errors.has('domain')">{{ errors.first('name') }}</span>
+                        <span class="help-block" v-show="errors.has('domain')">网站域名不能为空</span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('beian') }">
@@ -64,7 +64,7 @@
                         <input name="beian" type="text" class="form-control" placeholder="请输入备案信息" v-model="beian" v-validate data-vv-rules="required">
                     </div>
                     <div class="col-sm-5">
-                        <span class="help-block" v-show="errors.has('beian')">{{ errors.first('beian') }}</span>
+                        <span class="help-block" v-show="errors.has('beian')">备案信息不能为空</span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('company') }">
@@ -73,7 +73,7 @@
                         <input name="company" type="text" class="form-control" placeholder="请输入公司名称" v-model="company" v-validate data-vv-rules="required">
                     </div>
                     <div class="col-sm-5">
-                        <span class="help-block" v-show="errors.has('company')">{{ errors.first('company') }}</span>
+                        <span class="help-block" v-show="errors.has('company')">公司名称不能为空</span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('copyright') }">
@@ -82,7 +82,7 @@
                         <input name="copyright" type="text" class="form-control" placeholder="请输入版权信息" v-model="copyright" v-validate data-vv-rules="required">
                     </div>
                     <div class="col-sm-5">
-                        <span class="help-block" v-show="errors.has('copyright')">{{ errors.first('copyright') }}</span>
+                        <span class="help-block" v-show="errors.has('copyright')">版权信息不能为空</span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('statistics') }">
@@ -91,7 +91,7 @@
                         <textarea name="statistics" class="form-control" rows="6" placeholder="请输入统计代码"></textarea>
                     </div>
                     <div class="col-sm-5">
-                        <span class="help-block" v-show="errors.has('statistics')">{{ errors.first('statistics') }}</span>
+                        <span class="help-block" v-show="errors.has('statistics')">统计代码不能为空</span>
                     </div>
                 </div>
             </div>
