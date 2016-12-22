6455c60bb27f8c2e70179f81e50f61e9e90bf28f
twilroad <269044570@qq.com>
2016-12-22 20:13:18 +0800

    add error texts in component Mail

--- resources/assets/src/components/pages/Mail.vue
+++ resources/assets/src/components/pages/Mail.vue
@@ -29,11 +29,17 @@
                     <div class="col-sm-4">
                         <input name="protocol" type="text" class="form-control" placeholder="请输入协议" v-model="protocol" v-validate data-vv-rules="required">
                     </div>
+                    <div class="col-sm-5">
+                        <span class="help-block" v-show="errors.has('protocol')">协议不能为空</span>
+                    </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('domain') }">
                     <label class="col-sm-3 control-label">加密方式</label>
                     <div class="col-sm-4">
                         <input name="encryption" type="text" class="form-control" placeholder="请输入加密方式" v-model="encryption" v-validate data-vv-rules="required">
+                    </div>
+                    <div class="col-sm-5">
+                        <span class="help-block" v-show="errors.has('encryption')">加密方式不能为空</span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('domain') }">
@@ -41,11 +47,17 @@
                     <div class="col-sm-4">
                         <input name="host" type="text" class="form-control" placeholder="请输入主机地址" v-model="host" v-validate data-vv-rules="required">
                     </div>
+                    <div class="col-sm-5">
+                        <span class="help-block" v-show="errors.has('host')">主机地址不能为空</span>
+                    </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('domain') }">
                     <label class="col-sm-3 control-label">邮箱</label>
                     <div class="col-sm-4">
                         <input name="mail" type="text" class="form-control" placeholder="请输入邮箱名称" v-model="mail" v-validate data-vv-rules="required">
+                    </div>
+                    <div class="col-sm-5">
+                        <span class="help-block" v-show="errors.has('mail')">邮箱不能为空</span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('domain') }">
@@ -53,11 +65,17 @@
                     <div class="col-sm-4">
                         <input name="username" type="text" class="form-control" placeholder="请输入用户名称" v-model="username" v-validate data-vv-rules="required">
                     </div>
+                    <div class="col-sm-5">
+                        <span class="help-block" v-show="errors.has('username')">用户名不能为空</span>
+                    </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('domain') }">
                     <label class="col-sm-3 control-label">密码</label>
                     <div class="col-sm-4">
                         <input name="password" type="password" class="form-control" placeholder="请输入密码" v-model="password" v-validate data-vv-rules="required">
+                    </div>
+                    <div class="col-sm-5">
+                        <span class="help-block" v-show="errors.has('password')">密码不能为空</span>
                     </div>
                 </div>
             </div>
