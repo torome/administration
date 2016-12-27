eb3843f19ed12404830bec31fbb6d7b190019442
twilroad <269044570@qq.com>
2016-12-27 20:44:28 +0800

    reset form column's with in component Seo

--- resources/assets/src/components/pages/Seo.vue
+++ resources/assets/src/components/pages/Seo.vue
@@ -21,29 +21,29 @@
         <div class="form-horizontal">
             <div class="box-body">
                 <div class="form-group" :class="{ 'has-error': errors.has('domain') }">
-                    <label for="title" class="col-sm-3 control-label">标题</label>
-                    <div class="col-sm-4">
-                        <input name="title" id="title" type="text" class="form-control" placeholder="请输入标题" v-model="title" v-validate data-vv-rules="required">
+                    <label class="col-sm-1 control-label">标题</label>
+                    <div class="col-sm-3">
+                        <input name="title" type="text" class="form-control" placeholder="请输入标题" v-model="title" v-validate data-vv-rules="required">
                     </div>
-                    <div class="col-sm-5">
+                    <div class="col-sm-8">
                         <span class="help-block" v-show="errors.has('title')">标题不能为空</span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('domain') }">
-                    <label for="description" class="col-sm-3 control-label">描述</label>
-                    <div class="col-sm-4">
-                        <input name="description" id="description" type="text" class="form-control" placeholder="请输入描述" v-model="description" v-validate data-vv-rules="required">
+                    <label class="col-sm-1 control-label">描述</label>
+                    <div class="col-sm-3">
+                        <input name="description" type="text" class="form-control" placeholder="请输入描述" v-model="description" v-validate data-vv-rules="required">
                     </div>
-                    <div class="col-sm-5">
+                    <div class="col-sm-8">
                         <span class="help-block" v-show="errors.has('description')">描述不能为空</span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('domain') }">
-                    <label for="keyword" class="col-sm-3 control-label">关键字</label>
-                    <div class="col-sm-4">
-                        <input name="keyword" id="keyword" type="text" class="form-control" placeholder="请输入关键词名称" v-model="keyword" v-validate data-vv-rules="required">
+                    <label class="col-sm-1 control-label">关键字</label>
+                    <div class="col-sm-3">
+                        <input name="keyword" type="text" class="form-control" placeholder="请输入关键词名称" v-model="keyword" v-validate data-vv-rules="required">
                     </div>
-                    <div class="col-sm-5">
+                    <div class="col-sm-8">
                         <span class="help-block" v-show="errors.has('keyword')">关键字不能为空</span>
                     </div>
                 </div>
