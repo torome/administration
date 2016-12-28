9c9d029783536e69e32c85e16fd29a9b5898d15f
twilroad <269044570@qq.com>
2016-12-28 22:11:27 +0800

    add token's getters

--- resources/assets/src/stores/getters.js
+++ resources/assets/src/stores/getters.js
@@ -1,3 +1,11 @@
 export const message = state => state.message
 
 export const setting = state => state.setting
+
+export const token = state => state.token
+
+export const accessToken = state => state.token.access_token
+
+export const tokenExpiresIn = state => state.token.expires_in
+
+export const refreshToken = state => state.token.refresh_token
