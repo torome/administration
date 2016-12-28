897c86258c3b598ccd1f78ffe8098d7bb7c778ef
twilroad <269044570@qq.com>
2016-12-28 21:51:03 +0800

    add token post request

--- resources/assets/src/components/pages/Login.vue
+++ resources/assets/src/components/pages/Login.vue
@@ -17,6 +17,14 @@
         if (this.errors.any()) {
           return false
         }
+        this.$http.post('http://notadd.io/admin/token', {
+          name: this.username,
+          password: this.password
+        }).then(function (response) {
+          console.log(response)
+        }, function (response) {
+          alert('请求失败！')
+        })
       }
     }
   }
@@ -28,14 +36,12 @@
         <div class="login-box-body">
             <div class="form-horizontal">
                 <div class="form-group has-feedback" :class="{ 'has-error': errors.has('username') }">
-                    <input name="username" type="text" class="form-control" placeholder="请输入用户名" v-model="username"
-                           v-validate data-vv-rules="required">
+                    <input name="username" type="text" class="form-control" placeholder="请输入用户名" v-model="username" v-validate data-vv-rules="required">
                     <span class="glyphicon glyphicon-user form-control-feedback"></span>
                     <span class="help-block" v-show="errors.has('username')">用户名不能为空</span>
                 </div>
                 <div class="form-group has-feedback" :class="{ 'has-error': errors.has('password') }">
-                    <input name="password" type="password" class="form-control" placeholder="请输入密码" v-model="password"
-                           v-validate data-vv-rules="required">
+                    <input name="password" type="password" class="form-control" placeholder="请输入密码" v-model="password" v-validate data-vv-rules="required">
                     <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                     <span class="help-block" v-show="errors.has('password')">密码不能为空</span>
                 </div>
@@ -46,8 +52,7 @@
                 </div>
                 <div class="row">
                     <div class="col-xs-12">
-                        <button class="btn btn-primary btn-block btn-flat" :disabled="errors.any()" @click="submit">登录
-                        </button>
+                        <button class="btn btn-primary btn-block" :disabled="errors.any()" @click="submit">登录</button>
                     </div>
                 </div>
             </div>
