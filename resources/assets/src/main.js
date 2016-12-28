e9eaa483168dbc1b38e6e950916f8aac815ced87
twilroad <269044570@qq.com>
2016-12-28 17:09:31 +0800

    add eslint-enable no-new

--- resources/assets/src/main.js
+++ resources/assets/src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'font-awesome/less/font-awesome.less'
 import 'bootstrap/less/bootstrap.less'
 import 'slimscroll/example/ssmaster/jquery.slimscroll'
@@ -23,3 +21,4 @@ new Vue({
   store,
   render: h => h(App)
 })
+/* eslint-enable no-new */
