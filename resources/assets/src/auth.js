ac5f8950b21c780a623deadffd197ba68d60d014
twilroad <269044570@qq.com>
2017-01-09 18:53:10 +0800

    get global settings while check login status

--- resources/assets/src/auth.js
+++ resources/assets/src/auth.js
@@ -5,11 +5,16 @@ export default {
   logined () {
     let token = JSON.parse(window.localStorage.getItem('token'))
     if (token) {
-      console.log(token.expires_in)
       if (token.access_token && token.refresh_token) {
-        store.commit('token', token)
         Vue.http.headers.common['Accept'] = 'application/json'
-        Vue.http.headers.common['Authorization'] = 'Bearer ' + window.localStorage.getItem('access_token')
+        Vue.http.headers.common['Authorization'] = 'Bearer ' + token.access_token
+        store.commit('token', token)
+        let setting = store.state.setting
+        if (!setting.hasOwnProperty()) {
+          Vue.http.post(window.api + '/setting/all').then(response => {
+            store.commit('setting', response.body.data)
+          })
+        }
         return true
       } else {
         return false
