52de824046f4595c5f47dca2d68443facf7ae897
twilroad <269044570@qq.com>
2017-01-09 16:27:36 +0800

    add common headers while logined

--- resources/assets/src/auth.js
+++ resources/assets/src/auth.js
@@ -1,9 +1,19 @@
+import Vue from 'vue'
+import store from './stores'
+
 export default {
   logined () {
     let token = JSON.parse(window.localStorage.getItem('token'))
     if (token) {
-      window.alert(token.expires_in)
-      return !!(token.access_token && token.refresh_token)
+      console.log(token.expires_in)
+      if (token.access_token && token.refresh_token) {
+        store.commit('token', token)
+        Vue.http.headers.common['Accept'] = 'application/json'
+        Vue.http.headers.common['Authorization'] = 'Bearer ' + window.localStorage.getItem('access_token')
+        return true
+      } else {
+        return false
+      }
     } else {
       return false
     }
