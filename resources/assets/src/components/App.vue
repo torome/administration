65583d562c601cf787413680a4cd7bc67eec3d01
twilroad <269044570@qq.com>
2017-01-09 17:31:44 +0800

    add btn-switch style

--- resources/assets/src/components/App.vue
+++ resources/assets/src/components/App.vue
@@ -2,6 +2,22 @@
   export default {
   }
 </script>
+<style lang="css">
+    .btn-switch > .btn {
+        position: relative;
+    }
+    .btn-switch > .btn > input {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: block;
+        opacity: 0;
+        margin: 0;
+        padding: 0;
+    }
+</style>
 <template>
     <router-view></router-view>
 </template>
