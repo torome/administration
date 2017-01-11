cd258a195dbfaf0069804b32d5565f76aee7cd52
twilroad <269044570@qq.com>
2017-01-11 17:27:02 +0800

    add attributes computed and methods to components

--- resources/assets/src/components/pages/content/Article.vue
+++ resources/assets/src/components/pages/content/Article.vue
@@ -1,6 +1,16 @@
 <script>
   export default {
     created () {
+    },
+    computed: {
+    },
+    methods: {
+      submit: function (e) {
+        this.$validator.validateAll()
+        if (this.errors.any()) {
+          return false
+        }
+      }
     }
   }
 </script>
