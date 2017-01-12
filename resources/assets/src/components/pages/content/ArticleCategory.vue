a6bd98947161f892093e1897b8f696a3ea992a6e
twilroad <269044570@qq.com>
2017-01-12 17:08:57 +0800

    component Category and Tag rename to ArticleCategory and TagCategory

--- resources/assets/src/components/pages/content/ArticleCategory.vue
+++ resources/assets/src/components/pages/content/ArticleCategory.vue
@@ -21,16 +21,8 @@
             <h3 class="box-title">分类管理</h3>
         </div>
         <div class="box-body">
-            <div class="form-horizontal">
-                <div class="form-group">
-                    <label class="col-sm-1 control-label"></label>
-                    <div class="col-sm-3">
-                    </div>
-                </div>
-            </div>
         </div>
         <div class="box-footer">
-            <button class="btn btn-primary" :disabled="errors.any()" @click="submit">保存</button>
         </div>
     </div>
 </template>
