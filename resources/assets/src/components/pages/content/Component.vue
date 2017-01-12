cdad3b4627efbe8d91d004314d5b80aea0b6abb4
twilroad <269044570@qq.com>
2017-01-12 17:51:27 +0800

    extend computed attributes and form fields in component Component

--- resources/assets/src/components/pages/content/Component.vue
+++ resources/assets/src/components/pages/content/Component.vue
@@ -3,39 +3,105 @@
     created () {
     },
     computed: {
-      description: {
+      articleDescription: {
         get () {
-          return this.$store.state.setting['seo.description']
+          return this.$store.state.setting['content.seo.article.description']
         },
         set (value) {
           this.$store.commit('single', {
-            key: 'seo.description',
+            key: 'content.seo.article.description',
             value: value
           })
         }
       },
-      keyword: {
+      articleKeyword: {
         get () {
-          return this.$store.state.setting['seo.keyword']
+          return this.$store.state.setting['content.seo.article.keyword']
         },
         set (value) {
           this.$store.commit('single', {
-            key: 'seo.keyword',
+            key: 'content.seo.article.keyword',
             value: value
           })
         }
       },
-      title: {
+      articleTitle: {
         get () {
-          return this.$store.state.setting['seo.title']
+          return this.$store.state.setting['content.seo.article.title']
         },
         set (value) {
           this.$store.commit('single', {
-            key: 'seo.title',
+            key: 'content.seo.article.title',
             value: value
           })
         }
-      }
+      },
+      categoryDescription: {
+        get () {
+          return this.$store.state.setting['content.seo.category.description']
+        },
+        set (value) {
+          this.$store.commit('single', {
+            key: 'content.seo.category.description',
+            value: value
+          })
+        }
+      },
+      categoryKeyword: {
+        get () {
+          return this.$store.state.setting['content.seo.category.keyword']
+        },
+        set (value) {
+          this.$store.commit('single', {
+            key: 'content.seo.category.keyword',
+            value: value
+          })
+        }
+      },
+      categoryTitle: {
+        get () {
+          return this.$store.state.setting['content.seo.category.title']
+        },
+        set (value) {
+          this.$store.commit('single', {
+            key: 'content.seo.category.title',
+            value: value
+          })
+        }
+      },
+      pageDescription: {
+        get () {
+          return this.$store.state.setting['content.seo.page.description']
+        },
+        set (value) {
+          this.$store.commit('single', {
+            key: 'content.seo.page.description',
+            value: value
+          })
+        }
+      },
+      pageKeyword: {
+        get () {
+          return this.$store.state.setting['content.seo.page.keyword']
+        },
+        set (value) {
+          this.$store.commit('single', {
+            key: 'content.seo.page.keyword',
+            value: value
+          })
+        }
+      },
+      pageTitle: {
+        get () {
+          return this.$store.state.setting['content.seo.page.title']
+        },
+        set (value) {
+          this.$store.commit('single', {
+            key: 'content.seo.page.title',
+            value: value
+          })
+        }
+      },
     },
     methods: {
       submit: function (e) {
@@ -50,35 +116,103 @@
 <template>
     <div class="box box-solid">
         <div class="box-header with-border">
-            <h3 class="box-title">SEO管理</h3>
+            <h3 class="box-title">文章默认SEO配置</h3>
         </div>
         <div class="box-body">
             <div class="form-horizontal">
-                <div class="form-group" :class="{ 'has-error': errors.has('title') }">
+                <div class="form-group" :class="{ 'has-error': errors.has('articleTitle') }">
                     <label class="col-sm-1 control-label">标题</label>
                     <div class="col-sm-3">
-                        <input name="title" type="text" class="form-control" placeholder="请输入标题" v-model="title" v-validate data-vv-rules="required">
+                        <input name="articleTitle" type="text" class="form-control" placeholder="请输入标题" v-model="articleTitle" v-validate data-vv-rules="required">
                     </div>
                     <div class="col-sm-8">
-                        <span class="help-block" v-show="errors.has('title')">标题不能为空</span>
+                        <span class="help-block" v-show="errors.has('articleTitle')">标题不能为空</span>
                     </div>
                 </div>
-                <div class="form-group" :class="{ 'has-error': errors.has('description') }">
+                <div class="form-group" :class="{ 'has-error': errors.has('articleDescription') }">
                     <label class="col-sm-1 control-label">描述</label>
                     <div class="col-sm-3">
-                        <input name="description" type="text" class="form-control" placeholder="请输入描述" v-model="description" v-validate data-vv-rules="required">
+                        <input name="articleDescription" type="text" class="form-control" placeholder="请输入描述" v-model="articleDescription" v-validate data-vv-rules="required">
                     </div>
                     <div class="col-sm-8">
-                        <span class="help-block" v-show="errors.has('description')">描述不能为空</span>
+                        <span class="help-block" v-show="errors.has('articleDescription')">描述不能为空</span>
                     </div>
                 </div>
-                <div class="form-group" :class="{ 'has-error': errors.has('keyword') }">
+                <div class="form-group" :class="{ 'has-error': errors.has('articleKeyword') }">
                     <label class="col-sm-1 control-label">关键字</label>
                     <div class="col-sm-3">
-                        <input name="keyword" type="text" class="form-control" placeholder="请输入关键词名称" v-model="keyword" v-validate data-vv-rules="required">
+                        <input name="articleKeyword" type="text" class="form-control" placeholder="请输入关键词名称" v-model="articleKeyword" v-validate data-vv-rules="required">
                     </div>
                     <div class="col-sm-8">
-                        <span class="help-block" v-show="errors.has('keyword')">关键字不能为空</span>
+                        <span class="help-block" v-show="errors.has('articleKeyword')">关键字不能为空</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="box-header with-border">
+            <h3 class="box-title">分类默认SEO配置</h3>
+        </div>
+        <div class="box-body">
+            <div class="form-horizontal">
+                <div class="form-group" :class="{ 'has-error': errors.has('categoryTitle') }">
+                    <label class="col-sm-1 control-label">标题</label>
+                    <div class="col-sm-3">
+                        <input name="categoryTitle" type="text" class="form-control" placeholder="请输入标题" v-model="categoryTitle" v-validate data-vv-rules="required">
+                    </div>
+                    <div class="col-sm-8">
+                        <span class="help-block" v-show="errors.has('categoryTitle')">标题不能为空</span>
+                    </div>
+                </div>
+                <div class="form-group" :class="{ 'has-error': errors.has('categoryDescription') }">
+                    <label class="col-sm-1 control-label">描述</label>
+                    <div class="col-sm-3">
+                        <input name="categoryDescription" type="text" class="form-control" placeholder="请输入描述" v-model="categoryDescription" v-validate data-vv-rules="required">
+                    </div>
+                    <div class="col-sm-8">
+                        <span class="help-block" v-show="errors.has('categoryDescription')">描述不能为空</span>
+                    </div>
+                </div>
+                <div class="form-group" :class="{ 'has-error': errors.has('categoryKeyword') }">
+                    <label class="col-sm-1 control-label">关键字</label>
+                    <div class="col-sm-3">
+                        <input name="categoryKeyword" type="text" class="form-control" placeholder="请输入关键词名称" v-model="categoryKeyword" v-validate data-vv-rules="required">
+                    </div>
+                    <div class="col-sm-8">
+                        <span class="help-block" v-show="errors.has('categoryKeyword')">关键字不能为空</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="box-header with-border">
+            <h3 class="box-title">页面默认SEO配置</h3>
+        </div>
+        <div class="box-body">
+            <div class="form-horizontal">
+                <div class="form-group" :class="{ 'has-error': errors.has('pageTitle') }">
+                    <label class="col-sm-1 control-label">标题</label>
+                    <div class="col-sm-3">
+                        <input name="pageTitle" type="text" class="form-control" placeholder="请输入标题" v-model="pageTitle" v-validate data-vv-rules="required">
+                    </div>
+                    <div class="col-sm-8">
+                        <span class="help-block" v-show="errors.has('pageTitle')">标题不能为空</span>
+                    </div>
+                </div>
+                <div class="form-group" :class="{ 'has-error': errors.has('pageDescription') }">
+                    <label class="col-sm-1 control-label">描述</label>
+                    <div class="col-sm-3">
+                        <input name="pageDescription" type="text" class="form-control" placeholder="请输入描述" v-model="pageDescription" v-validate data-vv-rules="required">
+                    </div>
+                    <div class="col-sm-8">
+                        <span class="help-block" v-show="errors.has('pageDescription')">描述不能为空</span>
+                    </div>
+                </div>
+                <div class="form-group" :class="{ 'has-error': errors.has('pageKeyword') }">
+                    <label class="col-sm-1 control-label">关键字</label>
+                    <div class="col-sm-3">
+                        <input name="pageKeyword" type="text" class="form-control" placeholder="请输入关键词名称" v-model="pageKeyword" v-validate data-vv-rules="required">
+                    </div>
+                    <div class="col-sm-8">
+                        <span class="help-block" v-show="errors.has('pageKeyword')">关键字不能为空</span>
                     </div>
                 </div>
             </div>
