9bdfa9893a5279ceb67df3be2eac1a860f706998
twilroad <269044570@qq.com>
2017-01-12 15:50:34 +0800

    add toolbar to box-header and box-footer

--- resources/assets/src/components/pages/content/Article.vue
+++ resources/assets/src/components/pages/content/Article.vue
@@ -16,15 +16,29 @@
 </script>
 <style></style>
 <template>
-    <div class="box box-solid">
+    <div class="box">
         <div class="box-header with-border">
             <h3 class="box-title">文章管理</h3>
-        </div>
-        <div class="box-body">
-            <div class="form-horizontal">
-                <div class="form-group">
-                    <label class="col-sm-1 control-label"></label>
-                    <div class="col-sm-3">
+            <div class="pull-right clearfix">
+                <div class="box-search">
+                    <div class="input-group input-group-sm">
+                        <input class="form-control pull-right" placeholder="请输入附件大小" type="text">
+                        <div class="input-group-btn">
+                            <button class="btn btn-primary">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-extend">
+                    <div class="btn-group btn-group-sm">
+                        <button class="btn btn-primary">全选</button>
+                        <button class="btn btn-primary">反选</button>
+                    </div>
+                    <button class="btn btn-info btn-sm">回收站</button>
+                    <div class="btn-group btn-group-sm">
+                        <button class="btn btn-warning">删除</button>
+                        <button class="btn btn-danger">彻底删除</button>
                     </div>
                 </div>
             </div>
@@ -328,7 +342,29 @@
             </table>
         </div>
         <div class="box-footer">
-            <button class="btn btn-primary" :disabled="errors.any()" @click="submit">保存</button>
+            <div class="pull-right clearfix">
+                <div class="box-search">
+                    <div class="input-group input-group-sm">
+                        <input class="form-control pull-right" placeholder="请输入附件大小" type="text">
+                        <div class="input-group-btn">
+                            <button class="btn btn-primary">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-extend">
+                    <div class="btn-group btn-group-sm">
+                        <button class="btn btn-primary">全选</button>
+                        <button class="btn btn-primary">反选</button>
+                    </div>
+                    <button class="btn btn-info btn-sm">回收站</button>
+                    <div class="btn-group btn-group-sm">
+                        <button class="btn btn-warning">删除</button>
+                        <button class="btn btn-danger">彻底删除</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
