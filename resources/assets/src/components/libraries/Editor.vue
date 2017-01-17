134537f44e0f0a7e55bcf0891b9ff09b9e7c54b7
twilroad <269044570@qq.com>
2017-01-17 18:01:46 +0800

    add component Editor

---
+++ resources/assets/src/components/libraries/Editor.vue
@@ -0,0 +1,41 @@
+<script>
+  export default {
+    mounted () {
+      const style = global.document.createElement('link')
+      style.setAttribute('rel', 'stylesheet')
+      style.setAttribute('href', 'http://notadd.io/assets/ueditor/themes/default/css/ueditor.css')
+      this.$el.appendChild(style)
+      const config = window.document.createElement('script')
+      config.setAttribute('src', 'http://notadd.io/assets/ueditor/ueditor.config.js')
+      this.$el.appendChild(config)
+      config.onload = () => {
+        console.log('config')
+        const source = window.document.createElement('script')
+        source.setAttribute('src', 'http://notadd.io/assets/ueditor/ueditor.all.js')
+        this.$el.appendChild(source)
+        source.onload = () => {
+          const editor = window.document.createElement('script')
+          editor.setAttribute('id', 'ueditor')
+          this.$el.appendChild(editor)
+          const language = global.document.createElement('script')
+          language.setAttribute('src', 'http://notadd.io/assets/ueditor/lang/zh-cn/zh-cn.js')
+          this.$el.appendChild(language)
+          language.onload = () => {
+            let ue = window.UE.getEditor('ueditor')
+            ue.addEventListener('ready', function () {
+              ue.setContent(this.content)
+            })
+            ue.addListener('contentChange', () => {
+              this.$emit('change', ue.getContent())
+            })
+          }
+        }
+      }
+    },
+    props: {
+      content: ''
+    }
+  }
+</script>
+<style></style>
+<template></template>
