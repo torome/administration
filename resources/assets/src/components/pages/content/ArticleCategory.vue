fd668b35f31714548153c2a78d6c0e2ac7fe4588
twilroad <269044570@qq.com>
2017-01-28 22:44:10 +0800

    add category add/edit modal

--- resources/assets/src/components/pages/content/ArticleCategory.vue
+++ resources/assets/src/components/pages/content/ArticleCategory.vue
@@ -1,64 +1,61 @@
 <script>
+  import Modal from 'vue2-bootstrap-modal'
   export default {
-    computed: {
-      items: {
-        get () {
-          return [
-            {
-              text: '文章',
-              description: '这是一个文章',
-              color: '#cccccc'
-            },
-            {
-              text: '文章',
-              description: '这是一个文章',
-              color: '#cccccc'
-            },
-            {
-              text: '文章',
-              description: '这是一个文章',
-              color: '#cccccc'
-            },
-            {
-              text: '文章',
-              description: '这是一个文章',
-              color: '#cccccc'
-            },
-            {
-              text: '文章',
-              description: '这是一个文章',
-              color: '#cccccc'
-            },
-            {
-              text: '文章',
-              description: '这是一个文章',
-              color: '#cccccc'
-            },
-            {
-              text: '文章',
-              description: '这是一个文章',
-              color: '#cccccc'
-            },
-            {
-              text: '文章',
-              description: '这是一个文章',
-              color: '#cccccc'
-            },
-            {
-              text: '文章',
-              description: '这是一个文章',
-              color: '#cccccc'
-            },
-            {
-              text: '文章',
-              description: '这是一个文章',
-              color: '#cccccc'
-            }
-          ]
+    components: {
+      Modal
+    },
+    data () {
+      return {
+        items: [
+          {
+            text: '文章',
+            description: '这是一个文章',
+            color: '#cccccc'
+          },
+          {
+            text: '文章',
+            description: '这是一个文章',
+            color: '#cccccc'
+          },
+          {
+            text: '文章',
+            description: '这是一个文章',
+            color: '#cccccc'
+          }
+        ],
+        modal: {
+          color: '',
+          description: '',
+          link: '',
+          name: '',
+          title: '创建分类'
         }
       }
     },
     methods: {
+      create: function () {
+        this.items = [
+          {
+            text: '文章',
+            description: '这是一个文章',
+            color: '#cccccc'
+          },
+          {
+            text: '文章',
+            description: '这是一个文章',
+            color: '#cccccc'
+          }
+        ]
+        this.modal.color = ''
+        this.modal.description = ''
+        this.modal.link = ''
+        this.modal.name = ''
+        this.modal.title = '创建分类'
+        this.$refs.theModal.open()
+      },
+      edit: function (item) {
+        this.$refs.theModal.open()
+      },
       submit: function (e) {
         this.$validator.validateAll()
         if (this.errors.any()) {
@@ -67,10 +64,27 @@
       }
     },
     mounted () {
-      this.$store.commit('title', '分类管理 - 文章 - Notadd Administration')
-      this.$jquery(this.$el).find('ul, ol').sortable({
+      let _this = this
+      _this.$store.commit('title', '分类管理 - 文章 - Notadd Administration')
+      let sort = this.$jquery(this.$el).find('ul, ol').sortable({
         connectWith: 'category'
       })
+      _this.$jquery(sort).each(function (key, item) {
+        if (parseInt(key) === 0) {
+          _this.$jquery(item).on('sortupdate', function (e) {
+            let _detail = e.detail
+            console.log(_detail)
+          })
+        }
+      })
+    },
+    watch: {
+      modal: {
+        deep: true,
+        handler: function (val) {
+          console.log(val)
+        }
+      }
     }
   }
 </script>
@@ -79,10 +93,12 @@
         padding-bottom: 20px;
         padding-top: 20px;
     }
+
     .list-group > .list-group-item {
         border-width: 0;
         padding: 0;
     }
+
     .list-group > .list-group-item > .list-group-item-content {
         border-radius: 4px;
         cursor: move;
@@ -90,9 +106,11 @@
         line-height: 30px;
         margin-top: 5px;
     }
+
     .list-group > .list-group-item > .list-group-item-content:hover {
         background: #efefef;
     }
+
     .list-group > .list-group-item > .list-group-item-content > em {
         border-radius: 4px;
         float: left;
@@ -100,15 +118,44 @@
         margin: 7px;
         width: 16px;
     }
+
     .list-group > .list-group-item > .list-group-item-content:hover > em {
         opacity: 0;
     }
+
+    .list-group > .list-group-item > .list-group-item-content > .btn {
+        background: transparent;
+        border-radius: 4px 0 0 4px;
+        float: right;
+        opacity: 0;
+        padding: 5px 10px;
+    }
+
+    .list-group > .list-group-item > .list-group-item-content:hover > .btn {
+        opacity: 1;
+    }
+
     .list-group > .list-group-item > .list-group {
         margin-bottom: 0;
         padding-left: 26px;
     }
+
     ol.list-group {
         min-height: 5px;
+    }
+
+    .modal-content {
+        border: none;
+        box-shadow: none;
+    }
+
+    .modal-content > .form-group:last-child {
+        margin-bottom: 0;
+    }
+
+    .modal-footer {
+        border: none;
+        padding: 0;
     }
 </style>
 <template>
@@ -125,6 +172,7 @@
                                 <em :style="{ background: item.color }"></em>
                                 <span>{{ item.text }}</span>
                                 <i class="handle"></i>
+                                <button class="btn" @click="edit(item)"><i class="fa fa-fw fa-pencil"></i></button>
                             </div>
                             <ol class="list-group"></ol>
                         </li>
@@ -133,6 +181,37 @@
             </div>
         </div>
         <div class="box-footer">
+            <button class="btn btn-primary" @click="create">创建分类</button>
         </div>
+        <modal ref="theModal">
+            <div slot="title">
+                <div class="modal-title">{{ modal.title }}</div>
+            </div>
+            <div slot="body">
+                <div class="modal-content">
+                    <div class="form-group">
+                        <label>名称</label>
+                        <input class="form-control" v-model="modal.name">
+                    </div>
+                    <div class="form-group">
+                        <label>内链</label>
+                        <input class="form-control" v-model="modal.link">
+                    </div>
+                    <div class="form-group">
+                        <label>描述</label>
+                        <input class="form-control" v-model="modal.description">
+                    </div>
+                    <div class="form-group">
+                        <label>颜色</label>
+                        <input class="form-control" v-model="modal.color">
+                    </div>
+                </div>
+            </div>
+            <div slot="footer">
+                <div class="modal-footer">
+                    <button class="btn">保存</button>
+                </div>
+            </div>
+        </modal>
     </div>
 </template>
