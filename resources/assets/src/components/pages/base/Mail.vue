a52110ce2603a017dcf7787b78313b6dde033480
twilroad <269044570@qq.com>
2017-02-15 14:11:48 +0800

    实现发送方式和加密方式为选择的操作方式

--- resources/assets/src/components/pages/base/Mail.vue
+++ resources/assets/src/components/pages/base/Mail.vue
@@ -110,7 +110,26 @@
       }
     },
     mounted () {
-      this.$store.commit('title', '邮箱配置 - Notadd Administration')
+      let _this = this
+      _this.$store.commit('title', '邮箱配置 - Notadd Administration')
+      if (!_this.protocol) {
+        _this.protocol = 'mail'
+      } if (_this.protocol !== 'mail' && _this.protocol !== 'smtp') {
+        _this.protocol = 'mail'
+      }
+      if (!_this.encryption) {
+        _this.encryption = ''
+      }
+    },
+    updated () {
+      console.log('djfkdjf')
+      let _this = this
+      if (_this.encryption === '' && !_this.port) {
+        _this.port = 25
+      }
+      if (_this.encryption === 'ssl' && !_this.port) {
+        _this.port = 465
+      }
     }
   }
 </script>
@@ -125,8 +144,14 @@
                 <div class="form-group" :class="{ 'has-error': errors.has('protocol') }">
                     <label class="col-sm-1 control-label">发送方式</label>
                     <div class="col-sm-3">
-                        <input name="protocol" type="text" class="form-control" placeholder="发送方式" v-model="protocol"
-                               v-validate data-vv-rules="required">
+                        <div class="btn-group btn-switch">
+                            <label class="btn btn-primary" :class="{ active: protocol === 'mail' }">
+                                <input type="radio" v-model="protocol" value="mail"> Mail
+                            </label>
+                            <label class="btn btn-primary" :class="{ active: protocol === 'smtp' }">
+                                <input type="radio" v-model="protocol" value="smtp"> SMTP
+                            </label>
+                        </div>
                     </div>
                     <div class="col-sm-8">
                         <span class="help-block">mail或smtp，默认为mail</span>
@@ -136,27 +161,45 @@
                 <div class="form-group">
                     <label class="col-sm-1 control-label">加密方式</label>
                     <div class="col-sm-3">
-                        <input name="encryption" type="text" class="form-control" placeholder="请输入加密方式" v-model="encryption">
+                        <div class="btn-group btn-switch">
+                            <label class="btn btn-primary" :class="{ active: encryption === '' }">
+                                <input type="radio" v-model="encryption" value=""> 不加密
+                            </label>
+                            <label class="btn btn-primary" :class="{ active: encryption === 'ssl' }">
+                                <input type="radio" v-model="encryption" value="ssl"> SSL
+                            </label>
+                        </div>
                     </div>
                     <div class="col-sm-8">
-                        <span class="help-block">若要使用SSL协议请输入ssl，若不需要请不填</span>
+                        <span class="help-block">若要使用SSL协议请选择<strong>SLL</strong>，若不需要请选择<strong>不加密</strong></span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('host') }">
                     <label class="col-sm-1 control-label">服务器地址</label>
                     <div class="col-sm-3">
                         <input name="host" type="text" class="form-control" placeholder="请输入服务器地址" v-model="host"
-                               v-validate data-vv-rules="required">
+                               v-validate data-vv-rules="required" :disabled="protocol === 'mail'">
                     </div>
                     <div class="col-sm-8">
                         <span class="help-block" v-show="errors.has('host')">服务器地址不能为空</span>
+                    </div>
+                </div>
+                <div class="form-group" :class="{ 'has-error': errors.has('port') }">
+                    <label class="col-sm-1 control-label">服务器端口</label>
+                    <div class="col-sm-3">
+                        <input name="port" type="text" class="form-control" placeholder="请输入服务器端口" v-model="port"
+                               v-validate data-vv-rules="required" :disabled="protocol === 'mail'">
+                    </div>
+                    <div class="col-sm-8">
+                        <span class="help-block" v-show="errors.has('port')">不加密时端口默认为25，SSL加密时端口为465或587</span>
+                        <span class="help-block" v-show="errors.has('port')">服务器端口不能为空</span>
                     </div>
                 </div>
                 <div class="form-group" :class="{ 'has-error': errors.has('mail') }">
                     <label class="col-sm-1 control-label">发信邮箱地址</label>
                     <div class="col-sm-3">
                         <input name="mail" type="text" class="form-control" placeholder="请输入发信邮箱地址" v-model="mail"
-                               v-validate data-vv-rules="required">
+                               v-validate data-vv-rules="required" :disabled="protocol === 'mail'">
                     </div>
                     <div class="col-sm-8">
                         <span class="help-block" v-show="errors.has('mail')">发信邮箱地址不能为空</span>
@@ -165,7 +208,7 @@
                 <div class="form-group">
                     <label class="col-sm-1 control-label">SMTP账号</label>
                     <div class="col-sm-3">
-                        <input name="username" type="text" class="form-control" placeholder="请输入用户名称" v-model="username">
+                        <input name="username" type="text" class="form-control" placeholder="请输入用户名称" v-model="username" :disabled="protocol === 'mail'">
                     </div>
                     <div class="col-sm-8">
                         <span class="help-block">用于SMTP发送方式</span>
@@ -175,7 +218,7 @@
                     <label class="col-sm-1 control-label">SMTP密码</label>
                     <div class="col-sm-3">
                         <input name="password" type="password" class="form-control" placeholder="请输入密码"
-                               v-model="password" v-validate data-vv-rules="required">
+                               v-model="password" v-validate data-vv-rules="required" :disabled="protocol === 'mail'">
                     </div>
                     <div class="col-sm-8">
                         <span class="help-block">用于SMTP发送方式</span>
