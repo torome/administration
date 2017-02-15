db214c76b9ca7fb8926199237803a4d389d4315f
twilroad <269044570@qq.com>
2017-02-15 18:04:39 +0800

    add mail test logic

--- resources/assets/src/components/pages/base/Mail.vue
+++ resources/assets/src/components/pages/base/Mail.vue
@@ -1,13 +1,17 @@
 <script>
+  import Modal from '../../libraries/Modal'
   export default {
+    components: {
+      Modal
+    },
     computed: {
-      protocol: {
+      driver: {
         get () {
-          return this.$store.state.setting['mail.protocol']
+          return this.$store.state.setting['mail.driver']
         },
         set (value) {
           this.$store.commit('single', {
-            key: 'mail.protocol',
+            key: 'mail.driver',
             value: value
           })
         }
@@ -45,13 +49,13 @@
           })
         }
       },
-      mail: {
+      from: {
         get () {
-          return this.$store.state.setting['mail.mail']
+          return this.$store.state.setting['mail.from']
         },
         set (value) {
           this.$store.commit('single', {
-            key: 'mail.mail',
+            key: 'mail.from',
             value: value
           })
         }
@@ -79,22 +83,40 @@
         }
       }
     },
+    data () {
+      return {
+        modal: {
+          to: '',
+          content: ''
+        }
+      }
+    },
     methods: {
+      send: function () {
+        let _this = this
+        _this.$http.post(window.api + '/mail/test', {
+          to: _this.modal.to,
+          content: _this.modal.content
+        }).then(function (response) {
+          console.log(response.body)
+        }, function (response) {
+          console.log(response.body)
+        })
+      },
       submit: function (e) {
         let _this = this
-
-        _this.$validator.validateAll()
-
-        if (_this.errors.any()) {
-          return false
+        if (_this.driver === 'mail') {
+          _this.$validator.validateAll()
+          if (_this.errors.any()) {
+            return false
+          }
         }
-
         _this.$http.post(window.api + '/mail', {
-          protocol: _this.protocol,
+          driver: _this.driver,
           encryption: _this.encryption,
           port: _this.port,
           host: _this.host,
-          mail: _this.mail,
+          from: _this.from,
           username: _this.username,
           password: _this.password
         }).then(response => {
@@ -107,22 +129,25 @@
         }, response => {
           window.alert('更新设置失败！')
         })
+      },
+      test: function () {
+        this.$refs.modal.open()
       }
     },
     mounted () {
       let _this = this
       _this.$store.commit('title', '邮箱配置 - Notadd Administration')
-      if (!_this.protocol) {
-        _this.protocol = 'mail'
-      } if (_this.protocol !== 'mail' && _this.protocol !== 'smtp') {
-        _this.protocol = 'mail'
+      if (!_this.driver) {
+        _this.driver = 'mail'
+      }
+      if (_this.driver !== 'mail' && _this.driver !== 'smtp') {
+        _this.driver = 'mail'
       }
       if (!_this.encryption) {
         _this.encryption = ''
       }
     },
     updated () {
-      console.log('djfkdjf')
       let _this = this
       if (_this.encryption === '' && !_this.port) {
         _this.port = 25
@@ -133,7 +158,16 @@
     }
   }
 </script>
-<style></style>
+<style>
+    .modal-footer {
+        border: none;
+        padding: 0;
+    }
+
+    .modal-footer > .btn-submit {
+        float: left;
+    }
+</style>
 <template>
     <div class="box box-solid">
         <div class="box-header with-border">
@@ -141,21 +175,21 @@
         </div>
         <div class="box-body">
             <div class="form-horizontal">
-                <div class="form-group" :class="{ 'has-error': errors.has('protocol') }">
+                <div class="form-group" :class="{ 'has-error': errors.has('driver') }">
                     <label class="col-sm-1 control-label">发送方式</label>
                     <div class="col-sm-3">
                         <div class="btn-group btn-switch">
-                            <label class="btn btn-primary" :class="{ active: protocol === 'mail' }">
-                                <input type="radio" v-model="protocol" value="mail"> Mail
+                            <label class="btn btn-primary" :class="{ active: driver === 'mail' }">
+                                <input type="radio" v-model="driver" value="mail"> Mail
                             </label>
-                            <label class="btn btn-primary" :class="{ active: protocol === 'smtp' }">
-                                <input type="radio" v-model="protocol" value="smtp"> SMTP
+                            <label class="btn btn-primary" :class="{ active: driver === 'smtp' }">
+                                <input type="radio" v-model="driver" value="smtp"> SMTP
                             </label>
                         </div>
                     </div>
                     <div class="col-sm-8">
                         <span class="help-block">mail或smtp，默认为mail</span>
-                        <span class="help-block" v-show="errors.has('protocol')">必需填写发送方式</span>
+                        <span class="help-block" v-show="errors.has('driver')">必需填写发送方式</span>
                     </div>
                 </div>
                 <div class="form-group">
@@ -178,7 +212,7 @@
                     <label class="col-sm-1 control-label">服务器地址</label>
                     <div class="col-sm-3">
                         <input name="host" type="text" class="form-control" placeholder="请输入服务器地址" v-model="host"
-                               v-validate data-vv-rules="required" :disabled="protocol === 'mail'">
+                               v-validate data-vv-rules="required" :disabled="driver === 'mail'">
                     </div>
                     <div class="col-sm-8">
                         <span class="help-block" v-show="errors.has('host')">服务器地址不能为空</span>
@@ -188,27 +222,27 @@
                     <label class="col-sm-1 control-label">服务器端口</label>
                     <div class="col-sm-3">
                         <input name="port" type="text" class="form-control" placeholder="请输入服务器端口" v-model="port"
-                               v-validate data-vv-rules="required" :disabled="protocol === 'mail'">
+                               v-validate data-vv-rules="required" :disabled="driver === 'mail'">
                     </div>
                     <div class="col-sm-8">
                         <span class="help-block" v-show="errors.has('port')">不加密时端口默认为25，SSL加密时端口为465或587</span>
                         <span class="help-block" v-show="errors.has('port')">服务器端口不能为空</span>
                     </div>
                 </div>
-                <div class="form-group" :class="{ 'has-error': errors.has('mail') }">
+                <div class="form-group">
                     <label class="col-sm-1 control-label">发信邮箱地址</label>
                     <div class="col-sm-3">
-                        <input name="mail" type="text" class="form-control" placeholder="请输入发信邮箱地址" v-model="mail"
-                               v-validate data-vv-rules="required" :disabled="protocol === 'mail'">
+                        <input name="from" type="text" class="form-control" placeholder="请输入发信邮箱地址" v-model="from" :disabled="driver === 'mail'">
                     </div>
                     <div class="col-sm-8">
-                        <span class="help-block" v-show="errors.has('mail')">发信邮箱地址不能为空</span>
+                        <span class="help-block" v-show="errors.has('from')">发信邮箱地址不能为空</span>
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-sm-1 control-label">SMTP账号</label>
                     <div class="col-sm-3">
-                        <input name="username" type="text" class="form-control" placeholder="请输入用户名称" v-model="username" :disabled="protocol === 'mail'">
+                        <input name="username" type="text" class="form-control" placeholder="请输入用户名称" v-model="username"
+                               :disabled="driver === 'mail'">
                     </div>
                     <div class="col-sm-8">
                         <span class="help-block">用于SMTP发送方式</span>
@@ -218,7 +252,7 @@
                     <label class="col-sm-1 control-label">SMTP密码</label>
                     <div class="col-sm-3">
                         <input name="password" type="password" class="form-control" placeholder="请输入密码"
-                               v-model="password" v-validate data-vv-rules="required" :disabled="protocol === 'mail'">
+                               v-model="password" v-validate data-vv-rules="required" :disabled="driver === 'mail'">
                     </div>
                     <div class="col-sm-8">
                         <span class="help-block">用于SMTP发送方式</span>
@@ -228,6 +262,26 @@
         </div>
         <div class="box-footer">
             <button class="btn btn-primary btn-submit" :disabled="errors.any()" @click="submit">保存</button>
+            <button class="btn btn-submit" @click="test">测试邮件发送</button>
         </div>
+        <modal ref="modal">
+            <div slot="title">
+                <div class="modal-title"><strong>邮件发送测试</strong></div>
+            </div>
+            <div slot="body">
+                <p>请在更新邮件参数配置后在进行测试</p>
+                <div class="form-group">
+                    <label>邮箱地址</label>
+                    <input class="form-control" v-model="modal.to">
+                </div>
+                <div class="form-group">
+                    <label>邮件内容</label>
+                    <textarea class="form-control" rows="10" v-model="modal.content"></textarea>
+                </div>
+                <div class="modal-footer clearfix">
+                    <button class="btn btn-primary btn-submit" @click="send">保存</button>
+                </div>
+            </div>
+        </modal>
     </div>
 </template>
