190c8f44d009e1bef8bfb6f967e10e78c161f670
twilroad <269044570@qq.com>
2017-03-15 18:52:54 +0800

    add component and router for debug page

--- resources/mixes/view/src/router/index.js
+++ resources/mixes/view/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import Debug from '../pages/Debug';
 import Layout from '../layouts/Layout';
 import Login from '../pages/Login';
 import Setting from '../pages/Setting';
@@ -8,6 +9,10 @@ import Setting from '../pages/Setting';
 Vue.use(Router);
 
 const configuration = [
+  {
+    component: Debug,
+    path: 'debug',
+  },
   {
     component: Setting,
     path: 'setting',
