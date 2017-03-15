88db1c55fe93eb9c066b8de471b1ec33386009b5
twilroad <269044570@qq.com>
2017-03-15 19:34:45 +0800

    add template codes for sitemap page

--- resources/mixes/view/src/pages/Sitemap.vue
+++ resources/mixes/view/src/pages/Sitemap.vue
@@ -5,4 +5,34 @@
       },
     };
 </script>
-<template></template>
+<template>
+    <i-form :label-width="150">
+        <form-item label="生成 XML 地图">
+            <i-col span="9">
+                <radio-group>
+                    <radio :value="true">开启</radio>
+                    <radio :value="false">关闭</radio>
+                </radio-group>
+            </i-col>
+        </form-item>
+        <form-item label="生成 HTML 地图">
+            <i-col span="9">
+                <radio-group>
+                    <radio :value="true">开启</radio>
+                    <radio :value="false">关闭</radio>
+                </radio-group>
+            </i-col>
+        </form-item>
+        <form-item label="只包含最近的文章">
+            <i-col span="9">
+                <radio-group>
+                    <radio :value="true">开启</radio>
+                    <radio :value="false">关闭</radio>
+                </radio-group>
+            </i-col>
+        </form-item>
+        <form-item>
+            <i-button type="primary" @click.native="submit">确认提交</i-button>
+        </form-item>
+    </i-form>
+</template>
