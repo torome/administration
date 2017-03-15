5685d360f666d35df91e07839845ba8c77ecc81e
twilroad <269044570@qq.com>
2017-03-15 12:45:37 +0800

    allow console during development

--- resources/mixes/view/.eslintrc.js
+++ resources/mixes/view/.eslintrc.js
@@ -33,6 +33,8 @@ module.exports = {
     'import/no-extraneous-dependencies': ['error', {
       'optionalDependencies': ['test/unit/index.js']
     }],
+    // allow console during development
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
