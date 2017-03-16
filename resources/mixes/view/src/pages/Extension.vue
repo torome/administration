542a0e1af9e40fa571d34f2adb9590c270af1cd6
twilroad <269044570@qq.com>
2017-03-16 19:03:43 +0800

    add data binding and add event handler for extension page

--- resources/mixes/view/src/pages/Extension.vue
+++ resources/mixes/view/src/pages/Extension.vue
@@ -1,15 +1,53 @@
 <script>
     export default {
-
+        data() {
+            return {
+                columns: [
+                    {
+                        key: 'name',
+                        title: '插件名称',
+                        width: 200,
+                    },
+                    {
+                        key: 'author',
+                        title: '作者',
+                        width: 200,
+                    },
+                    {
+                        key: 'description',
+                        title: '描述',
+                    },
+                    {
+                        key: 'handle',
+                        render(row, column, index) {
+                            return `<i-switch v-model="list[${index}].enabled" @on-change="statusChanged(${index})"></i-switch>`;
+                        },
+                        title: '操作',
+                        width: 200,
+                    },
+                ],
+                list: [
+                    {
+                        author: 'Notadd <notadd@ibenchu.com>',
+                        enabled: true,
+                        description: '百度搜索插件',
+                        name: '百度搜索',
+                    },
+                ],
+                self: this,
+            };
+        },
+        methods: {
+            statusChanged(index) {
+                const self = this;
+                console.log(self.list[index]);
+            },
+        },
     };
 </script>
 <template>
     <card>
         <p slot="title">插件管理</p>
-        <i-form :label-width="100">
-            <form-item>
-                <i-button type="primary" @click.native="submit">确认提交</i-button>
-            </form-item>
-        </i-form>
+        <i-table :columns="columns" :content="self" :data="list" :show-header="false"></i-table>
     </card>
 </template>
