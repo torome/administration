fa478430b44f929c26c4b6ebe8b68e6fef1f0887
twilroad <269044570@qq.com>
2017-03-16 15:09:02 +0800

    add state mapping for upload page

--- resources/mixes/view/src/pages/Upload.vue
+++ resources/mixes/view/src/pages/Upload.vue
@@ -1,7 +1,9 @@
 <script>
+    import state from '../states/upload';
+
     export default {
-        data() {
-            return {};
+        computed: {
+            ...state,
         },
     };
 </script>
@@ -11,50 +13,60 @@
         <i-form :label-width="150">
             <form-item label="图片处理引擎">
                 <i-col span="9">
-                    <radio-group>
+                    <radio-group v-model="imageProcessingEngine">
                         <radio value="gd">GD 库</radio>
                     </radio-group>
                 </i-col>
             </form-item>
             <form-item label="附件大小">
                 <i-col span="9">
-                    <i-input placeholder="请输入附件大小">
+                    <i-input placeholder="请输入附件大小" v-model="fileMaxSize">
                         <span slot="append">KB</span>
                     </i-input>
                 </i-col>
             </form-item>
             <form-item label="图片大小">
                 <i-col span="9">
-                    <i-input placeholder="请输入图片大小">
+                    <i-input placeholder="请输入图片大小" v-model="imageMaxSize">
                         <span slot="append">KB</span>
                     </i-input>
                 </i-col>
             </form-item>
             <form-item label="视频大小">
                 <i-col span="9">
-                    <i-input placeholder="请输入视频大小">
+                    <i-input placeholder="请输入视频大小" v-model="videoMaxSize">
                         <span slot="append">KB</span>
                     </i-input>
                 </i-col>
             </form-item>
-            <form-item label="允许上传的扩展名">
+            <form-item label="允许图片的扩展名">
                 <i-col span="9">
-                    <i-input type="textarea" placeholder="请输入允许管理图片的扩展名" :autosize="{minRows: 2,maxRows: 5}"></i-input>
+                    <i-input type="textarea" placeholder="请输入允许管理图片的扩展名" v-model="canUploadImageExtension" :autosize="{minRows: 2,maxRows: 5}"></i-input>
                 </i-col>
             </form-item>
-            <form-item label="允许上传的扩展名">
+            <form-item label="允许上传截图的扩展名">
                 <i-col span="9">
-                    <i-input type="textarea" placeholder="请输入允许管理图片的扩展名" :autosize="{minRows: 2,maxRows: 5}"></i-input>
+                    <i-input type="textarea" placeholder="请输入允许管理截图的扩展名" v-model="canUploadCatcherExtension" :autosize="{minRows: 2,maxRows: 5}"></i-input>
+                </i-col>
+            </form-item>
+            <form-item label="允许上传视频的扩展名">
+                <i-col span="9">
+                    <i-input type="textarea" placeholder="请输入允许上传视频的扩展名" v-model="canUploadVideoExtension" :autosize="{minRows: 2,maxRows: 5}"></i-input>
+                </i-col>
+            </form-item>
+            <form-item label="允许上传文件的扩展名">
+                <i-col span="9">
+                    <i-input type="textarea" placeholder="请输入允许上传文件的扩展名" v-model="canUploadFileExtension" :autosize="{minRows: 2,maxRows: 5}"></i-input>
                 </i-col>
             </form-item>
             <form-item label="允许管理图片的扩展名">
                 <i-col span="9">
-                    <i-input type="textarea" placeholder="请输入允许管理图片的扩展名" :autosize="{minRows: 2,maxRows: 5}"></i-input>
+                    <i-input type="textarea" placeholder="请输入允许管理图片的扩展名" v-model="canManagementImageExtension" :autosize="{minRows: 2,maxRows: 5}"></i-input>
                 </i-col>
             </form-item>
             <form-item label="允许管理文件的扩展名">
                 <i-col span="9">
-                    <i-input type="textarea" placeholder="请输入允许管理文件的扩展名" :autosize="{minRows: 2,maxRows: 5}"></i-input>
+                    <i-input type="textarea" placeholder="请输入允许管理文件的扩展名" v-model="canManagementFileExtension" :autosize="{minRows: 2,maxRows: 5}"></i-input>
                 </i-col>
             </form-item>
             <form-item>
