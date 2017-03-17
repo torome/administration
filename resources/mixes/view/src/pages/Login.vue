4ce6ea2d8e47b45e10601285f8cfb7bc1e1f3102
twilroad <269044570@qq.com>
2017-03-17 14:33:40 +0800

    add validate rules for login form

--- resources/mixes/view/src/pages/Login.vue
+++ resources/mixes/view/src/pages/Login.vue
@@ -6,23 +6,49 @@
                     password: '',
                     username: '',
                 },
+                rules: {
+                    password: [
+                        {
+                            required: true,
+                            type: 'string',
+                            message: '请输入密码',
+                            trigger: 'change',
+                        },
+                    ],
+                    username: [
+                        {
+                            required: true,
+                            type: 'string',
+                            message: '请输入账号',
+                            trigger: 'change',
+                        },
+                    ],
+                },
             };
         },
         methods: {
             submit() {
-                console.log('Submit!');
+                const self = this;
+                self.$refs.form.validate(valid => {
+                    if (valid) {
+                    } else {
+                        this.$notice.error({
+                            title: '请正确填写账号或密码！',
+                        });
+                    }
+                });
             },
         },
     };
 </script>
 <template>
     <div class="login-layout">
-        <i-form :model="form" :label-width="100">
-            <form-item label="账号">
-                <i-input v-model="form.username" placeholder="请输入账号"></i-input>
+        <i-form :model="form" :label-width="0" ref="form" :rules="rules">
+            <form-item prop="username">
+                <i-input icon="person" v-model="form.username" placeholder="请输入账号"></i-input>
             </form-item>
-            <form-item label="密码">
-                <i-input v-model="form.username" placeholder="请输入密码"></i-input>
+            <form-item prop="password">
+                <i-input icon="locked" type="password" v-model="form.password" placeholder="请输入密码"></i-input>
             </form-item>
             <form-item>
                 <i-button type="primary" @click.native="submit">确认提交</i-button>
