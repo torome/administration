63f803c01a55da3ecfd040b87cc640eca451fbaf
twilroad <269044570@qq.com>
2017-03-18 17:58:30 +0800

    add logic for sitemap page

--- resources/mixes/view/src/pages/Sitemap.vue
+++ resources/mixes/view/src/pages/Sitemap.vue
@@ -1,23 +1,42 @@
 <script>
-    import state from '../states/sitemap';
+    import injection from '../helpers/injection';
 
     export default {
-        computed: {
-            ...state,
+        beforeRouteEnter(to, from, next) {
+            injection.loading.start();
+            injection.http.post(`${window.api}/sitemap/get`).then(response => {
+                const data = response.data.data;
+                next(vm => {
+                    injection.loading.finish();
+                    vm.form.cycle = data.cycle;
+                    vm.form.html = data.html;
+                    vm.form.recently = data.recently;
+                    vm.form.xml = data.xml;
+                });
+            });
         },
         data() {
             return {
                 form: {
-
+                    cycle: false,
+                    html: false,
+                    recently: false,
+                    xml: false,
                 },
-                rules: {
-
-                },
+                loading: false,
             };
         },
         methods: {
             submit() {
-                console.log('Submit!');
+                const self = this;
+                self.loading = true;
+                self.$http.post(`${window.api}/sitemap/set`, self.form).then(() => {
+                    self.$notice.open({
+                        title: '更新网站地图设置信息成功！',
+                    });
+                }).finally(() => {
+                    self.loading = false;
+                });
             },
         },
     };
@@ -25,41 +44,44 @@
 <template>
     <card>
         <p slot="title">网站地图</p>
-        <i-form :label-width="200" :model="form" ref="form" :rules="rules">
+        <i-form :label-width="200" :model="form">
             <row>
                 <i-col span="14">
-                    <form-item label="生成 XML 地图" prop="">
-                        <radio-group v-model="xml">
-                            <radio label="open">开启</radio>
-                            <radio label="close">关闭</radio>
-                        </radio-group>
+                    <form-item label="生成 XML 地图" prop="xml">
+                        <i-switch v-model="form.xml" size="large">
+                            <span slot="open">开启</span>
+                            <span slot="close">关闭</span>
+                        </i-switch>
                     </form-item>
                 </i-col>
             </row>
             <row>
                 <i-col span="14">
-                    <form-item label="生成 HTML 地图" prop="">
-                        <radio-group v-model="html">
-                            <radio label="open">开启</radio>
-                            <radio label="close">关闭</radio>
-                        </radio-group>
+                    <form-item label="生成 HTML 地图" prop="html">
+                        <i-switch v-model="form.html" size="large">
+                            <span slot="open">开启</span>
+                            <span slot="close">关闭</span>
+                        </i-switch>
                     </form-item>
                 </i-col>
             </row>
             <row>
                 <i-col span="14">
-                    <form-item label="只包含最近的文章" prop="">
-                        <radio-group v-model="recently">
-                            <radio label="open">开启</radio>
-                            <radio label="close">关闭</radio>
-                        </radio-group>
+                    <form-item label="只包含最近的文章" prop="recently">
+                        <i-switch v-model="form.recently" size="large">
+                            <span slot="open">开启</span>
+                            <span slot="close">关闭</span>
+                        </i-switch>
                     </form-item>
                 </i-col>
             </row>
             <row>
                 <i-col span="14">
                     <form-item>
-                        <i-button type="primary" @click.native="submit">确认提交</i-button>
+                        <i-button :loading="loading" type="primary" @click.native="submit">
+                            <span v-if="!loading">确认提交</span>
+                            <span v-else>正在提交…</span>
+                        </i-button>
                     </form-item>
                 </i-col>
             </row>
