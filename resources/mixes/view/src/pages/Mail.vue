40c3c56b9784a13b3dd23985a4551082aaaca369
twilroad <269044570@qq.com>
2017-03-19 16:56:47 +0800

    add test logic for mail page

--- resources/mixes/view/src/pages/Mail.vue
+++ resources/mixes/view/src/pages/Mail.vue
@@ -6,7 +6,6 @@
             injection.loading.start();
             injection.http.post(`${window.api}/mail/get`).then(response => {
                 const data = response.data.data;
-                console.log(data);
                 next(vm => {
                     injection.loading.finish();
                     vm.form.driver = data.driver;
@@ -30,14 +29,63 @@
                     username: '',
                     password: '',
                 },
+                loading: false,
+                mail: {
+                    address: '',
+                    content: '这是一封测试邮件！',
+                },
+                modal: {
+                    loading: true,
+                    visible: false,
+                },
+                rules: {
+                    address: [
+                        {
+                            required: true,
+                            type: 'email',
+                            message: '请输入正确的接收发信邮箱地址',
+                            trigger: 'change',
+                        },
+                    ],
+                },
             };
         },
         methods: {
             send() {
-
+                const self = this;
+                self.modal.loading = true;
+                self.$refs.form.validate(valid => {
+                    if (valid) {
+                        self.$http.post(`${window.api}/mail/test`, {
+                            content: self.mail.content,
+                            to: self.mail.address,
+                        }).then(() => {
+                            self.$notice.open({
+                                desc: self.mail.content,
+                                title: '发送测试邮件成功！内容如下：',
+                            });
+                        }).finally(() => {
+                            self.modal.loading = true;
+                            self.modal.visible = false;
+                        });
+                    } else {
+                        self.modal.loading = false;
+                        window.setTimeout(() => {
+                            self.modal.loading = true;
+                        }, 10);
+                    }
+                });
             },
             submit() {
-
+                const self = this;
+                self.loading = true;
+                injection.http.post(`${window.api}/mail`, self.form).then(() => {
+                    injection.notice.open({
+                        title: '更新邮件配置完成！',
+                    });
+                }).finally(() => {
+                    self.loading = false;
+                });
             },
         },
     };
@@ -50,8 +98,8 @@
                 <i-col span="14">
                     <form-item label="发送方式" prop="driver">
                         <radio-group v-model="form.driver">
-                            <radio label="mail"> Send Mail 函数 </radio>
-                            <radio label="smtp"> SMTP </radio>
+                            <radio label="mail"> Send Mail 函数</radio>
+                            <radio label="smtp"> SMTP</radio>
                         </radio-group>
                     </form-item>
                 </i-col>
@@ -60,8 +108,8 @@
                 <i-col span="14">
                     <form-item label="加密方式" prop="encryption">
                         <radio-group v-model="form.encryption">
-                            <radio label="none"> 不加密 </radio>
-                            <radio label="ssl"> SSL </radio>
+                            <radio label="none"> 不加密</radio>
+                            <radio label="ssl"> SSL</radio>
                         </radio-group>
                     </form-item>
                 </i-col>
@@ -69,46 +117,61 @@
             <row>
                 <i-col span="14">
                     <form-item label="服务器地址" prop="host">
-                        <i-input :disabled="form.driver !== 'smtp'" placeholder="请输入服务器地址" v-model="form.host"></i-input>
+                        <i-input :disabled="form.driver !== 'smtp'" placeholder="请输入服务器地址"
+                                 v-model="form.host"></i-input>
                     </form-item>
                 </i-col>
             </row>
             <row>
                 <i-col span="14">
                     <form-item label="服务器端口" prop="port">
-                        <i-input :disabled="form.driver !== 'smtp'" placeholder="请输入服务器端口" v-model="form.port"></i-input>
+                        <i-input :disabled="form.driver !== 'smtp'" placeholder="请输入服务器端口"
+                                 v-model="form.port"></i-input>
                     </form-item>
                 </i-col>
             </row>
             <row>
                 <i-col span="14">
                     <form-item label="发信邮箱地址" prop="from">
-                        <i-input :disabled="form.driver !== 'smtp'" placeholder="请输入发信邮箱地址" v-model="form.from"></i-input>
+                        <i-input :disabled="form.driver !== 'smtp'" placeholder="请输入发信邮箱地址"
+                                 v-model="form.from"></i-input>
                     </form-item>
                 </i-col>
             </row>
             <row>
                 <i-col span="14">
                     <form-item label="SMTP账号" prop="username">
-                        <i-input :disabled="form.driver !== 'smtp'" placeholder="请输入SMTP账号" v-model="form.username"></i-input>
+                        <i-input :disabled="form.driver !== 'smtp'" placeholder="请输入SMTP账号"
+                                 v-model="form.username"></i-input>
                     </form-item>
                 </i-col>
             </row>
             <row>
                 <i-col span="14">
                     <form-item label="SMTP密码" prop="password">
-                        <i-input :disabled="form.driver !== 'smtp'" placeholder="请输入SMTP密码" v-model="form.password"></i-input>
+                        <i-input :disabled="form.driver !== 'smtp'" placeholder="请输入SMTP密码"
+                                 v-model="form.password"></i-input>
                     </form-item>
                 </i-col>
             </row>
             <row>
                 <i-col span="14">
                     <form-item>
-                        <i-button type="primary" @click.native="submit">修改配置</i-button>
-                        <i-button @click.native="send">发送测试邮件</i-button>
+                        <i-button :loading="loading" type="primary" @click.native="submit">
+                            <span v-if="!loading">修改配置</span>
+                            <span v-else>正在提交…</span>
+                        </i-button>
+                        <i-button @click.native="modal.visible = true">发送测试邮件</i-button>
                     </form-item>
                 </i-col>
             </row>
         </i-form>
+        <modal :loading="modal.loading" title="发送测试邮件" :value="modal.visible" @on-cancel="modal.visible = false" @on-ok="send">
+            <i-form label-position="left" :model="mail" ref="form" :rules="rules">
+                <form-item label="接收邮箱地址" prop="address">
+                    <i-input placeholder="请输入接收发信邮箱地址" v-model="mail.address"></i-input>
+                </form-item>
+            </i-form>
+        </modal>
     </card>
 </template>
