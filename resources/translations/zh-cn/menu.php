86baa3d907439b19e7efaaee1981f6dc78a727fc
twilroad <269044570@qq.com>
2017-03-22 17:52:48 +0800

    add translation files for admin

---
+++ resources/translations/zh-cn/menu.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-03-22 17:47
+ */
+
+return [
+    'header'  => [],
+    'sidebar' => [],
+];
